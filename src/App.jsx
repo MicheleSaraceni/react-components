fb2021127e8b2ffe284f5ec7715bc674baa2c3aa
MicheleSaraceni <michele.saraceni23@email.com>
2024-12-12 16:54:08 +0100

    fixed sintax error

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,28 @@
 function App() {
-
   return (
     <>
       <header>
         <h1>Il mio blog</h1>
       </header>
       <main>
-        <div class="card" style="width: 18rem;">
-          <img src="..." class="card-img-top" alt="..." />
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+        <div className="card" style={{ width: "18rem" }}>
+          <img src="..." className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the bulk of the card's content.
+            </p>
+            <a href="#" className="btn btn-primary">Go somewhere</a>
           </div>
         </div>
       </main>
       <footer>
-        <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure praesentium aspernatur culpa illo aut quia voluptatum natus tempora facere laboriosam! Rem itaque consectetur culpa dolore, harum ipsam neque porro quaerat.</span>
+        <span>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure praesentium aspernatur culpa illo aut quia voluptatum natus tempora facere laboriosam! Rem itaque consectetur culpa dolore, harum ipsam neque porro quaerat.
+        </span>
       </footer>
     </>
   );
-};
+}
 
-export default App
+export default App;
