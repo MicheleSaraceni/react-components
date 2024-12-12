422b66195cfa6e0ed80d38cf1586a916094c4adb
MicheleSaraceni <michele.saraceni23@email.com>
2024-12-12 17:20:56 +0100

    ended layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,30 @@
 function App() {
   return (
     <>
-      <header>
-        <h1>Il mio blog</h1>
-      </header>
-      <main>
-        <div className="card" style={{ width: "18rem" }}>
-          <img src="..." className="card-img-top" alt="..." />
-          <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">
-              Some quick example text to build on the card title and make up the bulk of the card's content.
-            </p>
-            <a href="#" className="btn btn-primary">Go somewhere</a>
+      <div className="container d-flex flex-column align-items-center justify-content-between">
+        <header>
+          <h1>Il miei racconti</h1>
+        </header>
+        <main>
+
+          <div className="card" style={{ width: "18rem" }}>
+            <img src="../public/img/fantasy600x400.webp" className="card-img-top" alt="fantasy600x400" />
+            <div className="card-body">
+              <h5 className="card-title">Avventurieri fantasy</h5>
+              <p className="card-text">
+                Tipico gruppo di avventurieri fantasi in procinto di andare a sconfiggere un potente malvagio nel suo castello.
+              </p>
+              <a href="#" className="btn btn-primary">Vai alla storia</a>
+            </div>
           </div>
-        </div>
-      </main>
-      <footer>
-        <span>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure praesentium aspernatur culpa illo aut quia voluptatum natus tempora facere laboriosam! Rem itaque consectetur culpa dolore, harum ipsam neque porro quaerat.
-        </span>
-      </footer>
+
+        </main>
+        <footer>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure praesentium aspernatur culpa illo aut quia voluptatum natus tempora facere laboriosam! Rem itaque consectetur culpa dolore, harum ipsam neque porro quaerat.
+          </p>
+        </footer>
+      </div>
     </>
   );
 }
