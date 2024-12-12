d5c87b6f9984b3f51bfd6350a7e7d73656c144f7
MicheleSaraceni <michele.saraceni23@email.com>
2024-12-12 16:39:01 +0100

    added basic layout in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
-
-import './index.css'
-
 function App() {
 
   return (
     <>
-      <h1>Prova</h1>
+      <header>
+        <h1>Il mio blog</h1>
+      </header>
+      <main>
+        <div class="card" style="width: 18rem;">
+          <img src="..." class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </main>
+      <footer>
+        <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure praesentium aspernatur culpa illo aut quia voluptatum natus tempora facere laboriosam! Rem itaque consectetur culpa dolore, harum ipsam neque porro quaerat.</span>
+      </footer>
     </>
-  )
-}
+  );
+};
 
 export default App
